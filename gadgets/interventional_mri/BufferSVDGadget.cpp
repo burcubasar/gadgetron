57c25b61c6f89650cb088573a08f0fab362cd11a
burcubasar <63012801+burcubasar@users.noreply.github.com>
2020-04-28 18:41:22 -0400

    Add files via upload

--- gadgets/interventional_mri/BufferSVDGadget.cpp
+++ gadgets/interventional_mri/BufferSVDGadget.cpp
@@ -13,6 +13,26 @@ namespace {
     using namespace Gadgetron::Core;
 }
 
+namespace {
+
+    hoNDArray<float> combine_images(const std::deque<Image<float>>& image_buffer){
+        using namespace Gadgetron::Indexing;
+        auto [header, data, meta] = image_buffer.front();
+
+        std::vector<size_t> dimensions = data.dimensions();
+        dimensions.push_back(image_buffer.size());
+
+        hoNDArray<float> images(dimensions);
+        for (int i = 0; image_buffer.size(); i++){
+            auto [header, data, meta] = image_buffer.front();
+            images(slice,slice,slice,slice, i) = data;
+        }
+
+	return images;
+    }
+}
+  
+
 namespace Gadgetron::Core {
 
     BufferSVDGadget::BufferSVDGadget(
@@ -31,17 +51,12 @@ namespace Gadgetron::Core {
         auto [header,tmp,meta] = buffer.front();
 	int numFrames = buffer.size();
 	int numElems = tmp.size();
-	hoNDArray<float> data;
-	/*
-       	for (int x = 0; x<numFrames; x++){
-	  auto [header_,data_,meta_] = buffer[x];
-	  data[x*numElems] = data_;
-	}
-        GDEBUG("data dims: %d\n",data.size());
-        out.push(Image<float> {header,data_,meta});
-       */	
+	hoNDArray<float> collected_data = combine_images(buffer);
+  
+        GDEBUG("data dims: %d\n",collected_data.size());
+       	out.push(Image<float> {header,tmp,meta});
 
-	/*
+	/*	
 	for (int x = 0; x<buffer.size(); x++){
 	  for (int i = 0; i<tmp.size(); i++){
        	    auto [header_,data_,meta_] = buffer[x];
@@ -49,22 +64,14 @@ namespace Gadgetron::Core {
 	    }
 	}
 	
-      	GDEBUG("data dims: %d\n",tmp.size());
-        out.push(Image<float> {header,tmp,meta});
+	//	GDEBUG("data dims: %d\n",tmp.size());
+	// out.push(Image<float> {header,tmp,meta});
 	*/
 	
-	/*	 
-	  KLT.prepare(data, (size_t)1, (size_t)10, false);
-	  KLT.transform(data,PCs,(size_t)10);
-	  Image<float> image = {header,PCs,meta};
-          out.push(image);
-	  //  GDEBUG("data size: %d",data.size());
-	  */
-	  
-        auto [h,dt_,mt] = buffer.front();
-	out.push(Image<float> {h,dt_,mt});
-	GDEBUG("buffer dims: %d\n",buffer.size());  
-	
+		 
+	// KLT.prepare(collected_data, (size_t)1, (size_t)10, false);
+	  KLT.transform(collected_data,PCs,(size_t)10);
+ 
     }  
   }
 
