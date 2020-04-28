9a040eceec832eb0984e68b2195a89e6ba7a2675
burcubasar <63012801+burcubasar@users.noreply.github.com>
2020-04-28 18:44:08 -0400

    Add files via upload

--- gadgets/interventional_mri/FrameAccumulatorGadget.cpp
+++ gadgets/interventional_mri/FrameAccumulatorGadget.cpp
@@ -2,12 +2,34 @@
 #include "Context.h"
 #include "Channel.h"
 #include "Types.h"
-
 #include "hoNDArray.h"
+
+#include <deque>
+#include <tuple>
 
 namespace {
     using namespace Gadgetron;
     using namespace Gadgetron::Core;
+}
+
+namespace {
+
+    hoNDArray<float> combine_images(const std::deque<Image<float>>& image_buffer){
+        using namespace Gadgetron::Indexing;
+        auto [header, data, meta] = image_buffer.front();
+        
+        std::vector<size_t> dimensions = data.dimensions();
+        dimensions.push_back(image_buffer.size());
+
+        hoNDArray<float> images(dimensions);
+        for (int i = 0; image_buffer.size(); i++){
+            auto [header, data, meta] = image_buffer.front();
+            images(slice,slice,slice,slice, i) = data;
+        }
+
+        return images;
+
+    }    
 }
 
 namespace Gadgetron::Core {
@@ -19,19 +41,22 @@ namespace Gadgetron::Core {
 
   void FrameAccumulatorGadget::process(InputChannel<Image<float>> &in, OutputChannel &out) {
 
-        std::vector<Image<float>> buffer;
+        std::deque<Image<float>> buffer;
 
-	for (int x = 0; x<10; x++){
+	for (int x = 0; x<9; x++){
 	  buffer.emplace_back(in.pop());
      	}
 	
         for (auto image : in) {
             buffer.emplace_back(image);
-	  
-	    if (buffer.size() ==10) { GDEBUG("buffer size=1\n");
-                out.push(image);
+	    if (buffer.size() == 10){
+	       out.push(buffer);
+	      /*  hoNDArray<float> collected_buffer = combine_images(buffer);
+	       auto [header,im,meta] = buffer.front();
+	       out.push(Image<float> {header,collected_buffer,meta});*/
 	    }
-        }
+            buffer.pop_front();
+	}
   }
 
     GADGETRON_GADGET_EXPORT(FrameAccumulatorGadget);
